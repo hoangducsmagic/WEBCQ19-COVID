403bd68d8a93ced7b02b742ce56c0115b432952a
Lâm Hoàng Đức <hoangducsmagic@gmail.com>
2021-12-02 23:59:34 +0700

    get all patients function

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,5 +1,19 @@
-function dummy(req,res){
-    res.send("DUMMY FUNCTION")
+const {getQuery}=require('../models/db')
+
+async function dummy(req, res) {
+    var data = await getAllPatients();
+    console.log(data);
+    res.send("dummy")
+}
+
+async function getAllPatients() {
+    var query = `
+    SELECT p.patient_id as "patientId", p.name as "patientName", p.status as "patientStatus", f.name as "facilityName"
+    FROM patient p
+    JOIN facility f on p.current_facility_id=f.facility_id
+    `
+    var data = await getQuery(query);
+    return data;
 }
 
 module.exports={dummy}
