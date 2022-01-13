4fb90ad7bd8a7d524a73aa8f7c848c0cba70650c
ngoxuankien2000@gmail.com <ngoxuankien2000@gmail.com>
2022-01-13 20:12:16 +0700

    update total price of order

--- models/orderModel.js
+++ models/orderModel.js
@@ -2,7 +2,7 @@ const db = require('./db');
 
 async function showTotalPrice(orderId) {
     let query = `
-        SELECT SUM(p.price*p.unit) FROM order 
+        UPDATE SET total = SUM(p.price*p.unit) FROM order 
         INNER JOIN productpackage pp ON productpackage_id == pp.productpackage_id
         INNER JOIN productpackage_detal ppd on pp.productpackage_id == ppd.productpackage_id
         INNER JOIN product p on ppd.product_id == p.product_id
