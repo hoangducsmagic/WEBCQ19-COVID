5b3488ff293a2ca52df2b6ea3c7a186689d76ee7
KoahF <khoakg2013@gmail.com>
2022-01-15 21:40:14 +0700

    Add lock account function to admin

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,12 +1,16 @@
-const { Query } = require("pg");
-const db = require("./db");
+const { Query } = require('pg');
+const db = require('./db');
 
 async function lockUser(accountId) {
-    let query = `
-        UPDATE account SET locked = '${true}' from patient pa 
-        WHERE username = pa.citizen_id AND pa.patient_id = '`+accountId+`'`;
-    let data = await db.getQuery(query);
-    return data;
+	let query = `
+        UPDATE account acc
+        SET locked = '${true}' 
+        WHERE acc.username = '${accountId}'`;
+	// let query = `;
+	//     UPDATE account SET locked = '${true}' from patient pa
+	//     WHERE username = pa.citizen_id AND pa.patient_id = '`+accountId+`'`;
+	let data = await db.getQuery(query);
+	return data;
 }
 
-module.exports={lockUser}
+module.exports = { lockUser };
