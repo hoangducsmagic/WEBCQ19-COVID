4ee99dada459b71dcc781ad0909f1eda38239c9e
KoahF <khoakg2013@gmail.com>
2022-01-17 21:28:03 +0700

    Add pie chart (chartjs) to TotalCasesStats

--- controllers/statisticController.js
+++ controllers/statisticController.js
@@ -1,50 +1,55 @@
-const Statistic = require("../models/statisticModel");
+const Statistic = require('../models/statisticModel');
 
 async function showPackageConsumptionPage(req, res) {
-    var data = await Statistic.getPackageConsumption();
-    res.render("statistic/packageConsumption",{packageConsumptionList:data});
+	var data = await Statistic.getPackageConsumption();
+	res.render('statistic/packageConsumption', { packageConsumptionList: data });
 }
 
 async function showProductConsumptionPage(req, res) {
-    var data = await Statistic.getProductConsumption();
-    res.render("statistic/productConsumption", { productConsumptionList:data});
+	var data = await Statistic.getProductConsumption();
+	res.render('statistic/productConsumption', { productConsumptionList: data });
 }
 
 async function showTotalCasesPage(req, res) {
-    var dateFrom = req.query.inputFromDate;
-    var dateTo = req.query.inputToDate;
+	var dateFrom = req.query.inputFromDate;
+	var dateTo = req.query.inputToDate;
 
-    var data = await Statistic.getTotalCases(dateFrom, dateTo);
-
-    res.render("statistic/totalCases", { statusList: data, dateFrom, dateTo });
+	var data = await Statistic.getTotalCases(dateFrom, dateTo);
+	let chartData = data.map((item) => item['amount']).toString();
+	res.render('statistic/totalCases', {
+		statusList: data,
+		dateFrom,
+		dateTo,
+		chartData,
+	});
 }
 
 async function showStatusChangePage(req, res) {
-    var dateFrom = req.query.inputFromDate;
-    var dateTo = req.query.inputToDate;
+	var dateFrom = req.query.inputFromDate;
+	var dateTo = req.query.inputToDate;
 
-    var data = await Statistic.getStatusChangeData(dateFrom, dateTo);
+	var data = await Statistic.getStatusChangeData(dateFrom, dateTo);
 
-    res.render("statistic/statusChange", {
-        ...data,
-        dateFrom,
-        dateTo,
-    });
+	res.render('statistic/statusChange', {
+		...data,
+		dateFrom,
+		dateTo,
+	});
 }
 
-async function showPaymentStatisticPage(req,res){
-    const debtList=await Statistic.getDebtList();
-    const boughtList=await Statistic.getBoughtList();
-    res.render('statistic/debt',{
-        debtList,
-        boughtList
-    })
+async function showPaymentStatisticPage(req, res) {
+	const debtList = await Statistic.getDebtList();
+	const boughtList = await Statistic.getBoughtList();
+	res.render('statistic/debt', {
+		debtList,
+		boughtList,
+	});
 }
 
 module.exports = {
-    showPackageConsumptionPage,
-    showProductConsumptionPage,
-    showTotalCasesPage,
-    showStatusChangePage,
-    showPaymentStatisticPage
+	showPackageConsumptionPage,
+	showProductConsumptionPage,
+	showTotalCasesPage,
+	showStatusChangePage,
+	showPaymentStatisticPage,
 };
