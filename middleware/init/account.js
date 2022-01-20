4b3cc3306712f05a6f00d60d888ffec407c6b751
KoahF <khoakg2013@gmail.com>
2022-01-20 17:48:18 +0700

    Add check locked account to checkLogin()

--- middleware/init/account.js
+++ middleware/init/account.js
@@ -1,17 +1,18 @@
-exports.checkLogin = async (req, res, next) =>{
-    if (req.user) 
-    {
-        next();
-        return;
-    }
-    res.redirect('/account/login');
-}
+exports.checkLogin = async (req, res, next) => {
+	if (req.user) {
+		if (!req.user.locked) {
+			next();
+			return;
+		}
+		if (req.user.locked) req.logout();
+	}
+	res.redirect('/account/login');
+};
 
-exports.checkPaymenetLogin = async (req, res, next) =>{
-    if (req.cookies.token) 
-    {
-        next();
-        return;
-    }
-    res.redirect('/account/loginPayment');
-}
+exports.checkPaymenetLogin = async (req, res, next) => {
+	if (req.cookies.token) {
+		next();
+		return;
+	}
+	res.redirect('/account/loginPayment');
+};
